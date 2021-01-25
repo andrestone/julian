47f3ef940e89fa1f4a77686cc98d793187155f37
André <andrestone@users.noreply.github.com>
2021-01-25 20:38:22 +0100

    working

--- assets/lambda/index.ts
+++ assets/lambda/index.ts
@@ -7,9 +7,10 @@ const rds = new AWS.RDSDataService();
 export const handler: Handler = async(event, context, callback) => {
   console.log(event);
   // Call RDS
-  await rds.executeStatement({
+  const result = await rds.executeStatement({
     sql: 'select * from pg_catalog.pg_tables',
     secretArn: process.env['RDS_SECRET']!,
     resourceArn: process.env['RDS_CLUSTER_ARN']!
   }).promise()
+  console.log(result.records)
 }
